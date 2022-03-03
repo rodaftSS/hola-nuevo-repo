f693d738e0ca8b08cbaf916106368ea987b4e7ba
Salvador Garcia L <ing.salvadorgarcial@gmail.com>
2022-03-03 16:37:04 -0600

    add constructor y print

--- app/src/main/java/proyecto/Producto.java
+++ app/src/main/java/proyecto/Producto.java
@@ -3,11 +3,21 @@ package proyecto;
 public class Producto {
 
     int idProducto;
-    String Nombre;
-    String UnidadDeMedida;
+    String nombre;
+    String unidadDeMedida;
+
+
+    public Producto(){
+
+    }
+
+    public Producto(String nombre, String unidadDeMedida){
+        this.nombre = nombre;
+        this.unidadDeMedida = unidadDeMedida;
+    }
 
     public void mostrarDatosProducto(){
-        
+System.out.println(nombre + " " + unidadDeMedida);
     }
     
 }
