f254f5277edef405f9e968923d0acabc53b22073
th3cesar100 <ma3630075@gmail.com>
2022-03-03 14:41:39 -0600

    Agregue atributos apellido, vip, y getters and setters respectivos

--- app/src/main/java/proyecto/prueba/Cliente.java
+++ app/src/main/java/proyecto/prueba/Cliente.java
@@ -2,5 +2,33 @@ package proyecto.prueba;
 public class Cliente {
 
     String nombre;
+    private String apellido;
+    private boolean vip;
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+
+    }
+
+    public String getApellido(){
+        return this.apellido;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setVip(boolean vip){
+        this.vip = vip;
+    }
+
+    public boolean isVip(){
+        return this.vip;
+    }
+
     
 }
