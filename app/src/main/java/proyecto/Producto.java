2da76b22d1661173569b3355be291beb4273cbf1
Salvador Garcia L <ing.salvadorgarcial@gmail.com>
2022-03-03 16:30:27 -0600

    add Metodo

--- app/src/main/java/proyecto/Producto.java
+++ app/src/main/java/proyecto/Producto.java
@@ -3,5 +3,11 @@ package proyecto;
 public class Producto {
 
     int idProducto;
+    String Nombre;
+    String UnidadDeMedida;
+
+    public void mostrarDatosProducto(){
+        
+    }
     
 }
